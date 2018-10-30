d189c65b3c44a9d502944ea542d6c4a2ca0d1423
Joseph Venegas <lkoj>
2018-10-30 14:38:22 -0700

    more notes

--- notes/Basic Python Notes.py
+++ notes/Basic Python Notes.py
@@ -1,3 +1,4 @@
+"""
 print("Hello World!")
 
 # Apparently I'm going to slow, so i will speed up
@@ -39,9 +40,52 @@ car_mile_per_gallons = 0.01  # Float
 print("I have a car called %s. It's pretty nice." % car_name)
 print("It has %d cylinders, but get %f mpg" % (car_cylinders, car_miles_per_gallon))
 
+
+
 # Taking Input
 name = input("what is your name? ")
 print("Hello %s" % name)
 
 age = input("How old are you?")
 print("%s You belong in a museum!" % age)
+
+# Recasting
+real_age = input("How old are you again?")
+hidden_age = real_age + 5
+print(hidden_age)
+"""
+
+# Multi-Line Comments
+"""
+This is a multi-Line comment
+anything in between them is automatically commented out.
+"""
+
+
+# Defining Function
+def say_it():
+    print("Hello World")
+
+
+say_it()
+say_it()
+say_it()
+
+
+# f(x) = 2x + 3
+def f(x):
+    print(2*x + 3)
+
+
+f(1)
+f(5)
+f(5000)
+
+
+def distance(x1, y1, x2, y2):
+    dist = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+    print(dist)
+
+
+distance(0, 0, 3, 4)
+distance(0, 0, 5, 12)
---
+++ notes/Joseph Venegas.py
@@ -0,0 +1,4 @@
+adjective = input("Type in an adjective")
+noun = input("Type in a noun")
+
+print("I saw a %s %s" % (adjective, noun))
