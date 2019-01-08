9694f6db79892e080e09765f36804114cd34ac40
lkoj <42391755+lkoj@users.noreply.github.com>
2019-01-08 14:38:15 -0800

    ddd

---
+++ notes/semester 2 notes.py
@@ -0,0 +1,17 @@
+print("hello world")
+# Joseph Venegas
+
+cars = 5
+driving = True
+print("I have %d cars" % cars)
+print("I have " + str(cars) + "cars.")
+
+age = input("How old are you?")
+print("%s?? Really???" % age)
+
+
+
+colors = ["red", "blue", "black","yellow", "orenge"]
+colors.append("cyan")
+
+print(colors)
