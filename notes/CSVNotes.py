fe1dad52c55144e319e0327d80935aa1cd285562
lkoj <42391755+lkoj@users.noreply.github.com>
2019-04-25 14:38:33 -0700

    idk

--- notes/CSVNotes.py
+++ notes/CSVNotes.py
@@ -1,8 +1,23 @@
 import csv
 
+# with open("Book1.csv", 'r') as old_csv:
+#     reader = csv.reader(old_csv)
+#     for row in reader:
+#         old_number = row[0]
+#         # print(init(old_number) + 1)
+#         print(old_number)
+
 with open("Book1.csv", 'r') as old_csv:
-    reader = csv.reader(old_csv)
-    for row in reader:
-        old_number = row[0]
-        # print(init(old_number) + 1)
-        print(old_number)
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        print("Processing...")
+
+        for row in reader:
+            # old_number = int(row[0]) + 1
+            old_number = row[0]
+            first_num = int(old_number[0])
+            if first_num ==4:
+                writer.writerow(row)
+    print("Done")
+
