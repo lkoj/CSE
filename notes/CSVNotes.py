c2e0c8102f8dafb4ef40decddd13790c50dc901a
lkoj <42391755+lkoj@users.noreply.github.com>
2019-04-26 14:40:14 -0700

    idk

--- notes/CSVNotes.py
+++ notes/CSVNotes.py
@@ -1,5 +1,33 @@
 import csv
 
+def validate(num: str):
+    if is_first_num_odd(num) and is_second_num_even(num):
+        return True
+    return False
+
+
+def divisible_by_three(num: str):
+    first_num = int(num[0])
+    if first_num % 3 == 0:
+        return True
+    return False
+
+
+def disvisible_by_2(num: str):
+    first_num = int(num[0])
+    if first_num % 2 == 0:
+        return True
+    return False
+
+
+def is_first_num_odd(num: str):
+    first_num = int(num[0])
+    if first_num % 2 == 1:
+        return True
+    return False
+
+def is_second_num_even(num: str):
+
 # with open("Book1.csv", 'r') as old_csv:
 #     reader = csv.reader(old_csv)
 #     for row in reader:
@@ -7,17 +35,33 @@ import csv
 #         # print(init(old_number) + 1)
 #         print(old_number)
 
+# with open("Book1.csv", 'r') as old_csv:
+#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         print("Processing...")
+#
+#         for row in reader:
+#             # old_number = int(row[0]) + 1
+#             old_number = row[0]  # This is a string
+#             first_num = int(old_number[0])  # This is the first number
+#             if first_num == 4:
+#                 writer.writerow(row)
+#             # Print(int(old_number) + 1)
+#             # Print(old_number)
+#     print("Done")
+#
+
+
 with open("Book1.csv", 'r') as old_csv:
     with open("MyNewFile.csv", 'w', newline='') as new_csv:
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
-        print("Processing...")
-
         for row in reader:
             # old_number = int(row[0]) + 1
-            old_number = row[0]
-            first_num = int(old_number[0])
-            if first_num ==4:
+            old_number = row[0]  # This is a string
+            if validate(old_number):
                 writer.writerow(row)
-    print("Done")
-
+            # Print(int(old_number) + 1)
+            # Print(old_number)
+    print("OK")
