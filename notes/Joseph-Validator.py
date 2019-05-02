2938a9a64c67a3cf1b386fafcae04d91d7b0c490
lkoj <42391755+lkoj@users.noreply.github.com>
2019-05-02 14:25:54 -0700

    idk

--- notes/Joseph-Validator.py
+++ notes/Joseph-Validator.py
@@ -3,16 +3,21 @@ test_num = "6489319447155620"
 
 def validate(num: str):
     for index in range(len(num)):
-        int_version = int(num[index])
+        num_list[index] = int(num[index])
 
 
+num_list = list(test_num)
+print(num_list)
+print(len(num_list))
 
 print(validate(test_num))
 num_list = list(test_num)
-num_list.pop(15)
+num_list.pop()
 print(num_list)
 
 
 def reverse_test_num(string):
-    return string[::-15]
+    return string[::-1]
 
+
+print(reverse_test_num("6489319447155620"))
