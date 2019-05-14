f0ab51aa8a0e04ed6720ed0de9563f2626f8b602
lkoj <42391755+lkoj@users.noreply.github.com>
2019-05-14 14:38:20 -0700

    idk

--- notes/sales info.py
+++ notes/sales info.py
@@ -2,7 +2,22 @@ import csv
 
 with open("Sales Records.csv", "r") as csv_file_thing:
     reader = csv.reader(csv_file_thing)
+    fruits_sales = 0
+    clothes_sales = 0
+    meat_sales = 0
+    beverages_sales = 0
     for row in reader:
-        old_number = row[13]
-        print(old_number)
-        if
+        profits = row[13]
+        item_type = row[2]
+        if item_type == "Fruits":
+            fruits_sales += float(profits)
+        if item_type == "Clothes":
+            clothes_sales += float(profits)
+        if item_type == "Meat":
+            meat_sales += float(profits)
+        if item_type == "Beverages":
+            beverages_sales += float(profits)
+    print("Fruits", fruits_sales)
+    print("Clothes", clothes_sales)
+    print("Meat", meat_sales)
+    print()
