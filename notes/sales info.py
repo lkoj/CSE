06485ffd66a718948773fe93ea08f602359b06ed
lkoj <42391755+lkoj@users.noreply.github.com>
2019-05-15 14:38:03 -0700

    idk

--- notes/sales info.py
+++ notes/sales info.py
@@ -6,6 +6,14 @@ with open("Sales Records.csv", "r") as csv_file_thing:
     clothes_sales = 0
     meat_sales = 0
     beverages_sales = 0
+    office_supplies_sales = 0
+    cosmetics_sales = 0
+    snacks_sales = 0
+    personal_care_sales = 0
+    household_sales = 0
+    vegetables_sales = 0
+    baby_food_sales = 0
+    cereal_sales = 0
     for row in reader:
         profits = row[13]
         item_type = row[2]
@@ -17,7 +25,31 @@ with open("Sales Records.csv", "r") as csv_file_thing:
             meat_sales += float(profits)
         if item_type == "Beverages":
             beverages_sales += float(profits)
+        if item_type == "Office Supplies":
+            office_supplies_sales += float(profits)
+        if item_type == "Cosmetics":
+            cosmetics_sales += float(profits)
+        if item_type == "Snacks":
+            snacks_sales += float(profits)
+        if item_type == "Personal Care":
+            personal_care_sales += float(profits)
+        if item_type == "Household":
+            household_sales += float(profits)
+        if item_type == "Vegetables":
+            vegetables_sales += float(profits)
+        if item_type == "Baby Food":
+            baby_food_sales += float(profits)
+        if item_type == "Cereal":
+            cereal_sales += float(profits)
     print("Fruits", fruits_sales)
     print("Clothes", clothes_sales)
     print("Meat", meat_sales)
-    print()
+    print("Beverages", beverages_sales)
+    print("Office Supplies", office_supplies_sales)
+    print("Cosmetics", cosmetics_sales)
+    print("snacks", snacks_sales)
+    print("Personal Care", personal_care_sales)
+    print("Household", household_sales)
+    print("Vegetables", vegetables_sales)
+    print("Baby food", baby_food_sales)
+    print("Cereal", cereal_sales)
