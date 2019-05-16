98f9c9d9f9ac19e8866d3df11e4d9df7a213fd8c
lkoj <42391755+lkoj@users.noreply.github.com>
2019-05-16 14:38:44 -0700

    idk

--- notes/sales info.py
+++ notes/sales info.py
@@ -14,9 +14,17 @@ with open("Sales Records.csv", "r") as csv_file_thing:
     vegetables_sales = 0
     baby_food_sales = 0
     cereal_sales = 0
+    sub_saharan_africa_total = 0
+    middle_east_and_north_africa_total = 0
+    australia_and_oceania_total = 0
+    europe_total = 0
+    asia_total = 0
+    central_america_and_the_caribbean_total = 0
+    north_america_total = 0
     for row in reader:
         profits = row[13]
         item_type = row[2]
+        region = row[0]
         if item_type == "Fruits":
             fruits_sales += float(profits)
         if item_type == "Clothes":
@@ -41,6 +49,21 @@ with open("Sales Records.csv", "r") as csv_file_thing:
             baby_food_sales += float(profits)
         if item_type == "Cereal":
             cereal_sales += float(profits)
+        if region == "Sub saharan Africa":
+            sub_saharan_africa_total += float
+        if region == "Middle East and North Africa":
+            middle_east_and_north_africa_total += float
+        if region == "Australia and Oceania":
+            australia_and_oceania_total += float
+        if region == "Europe":
+            europe_total += float
+        if region == "Asia":
+            asia_total += float
+        if region == "Central America and the caribbean":
+            central_america_and_the_caribbean_total += float
+        if region == "North America":
+            north_america_total += float
+
     print("Fruits", fruits_sales)
     print("Clothes", clothes_sales)
     print("Meat", meat_sales)
@@ -53,3 +76,6 @@ with open("Sales Records.csv", "r") as csv_file_thing:
     print("Vegetables", vegetables_sales)
     print("Baby food", baby_food_sales)
     print("Cereal", cereal_sales)
+    print("Sub Saharan Africa", sub_saharan_africa_total)
+    print("Middle East and North Africa", middle_east_and_north_africa_total)
+    print("Australia and Oceania")
